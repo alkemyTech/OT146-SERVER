0cf06925ac0f553cc359b5675ec853dc74e075fe
Pablo Reyes <reyes_pabloeduardo@hotmail.com>
2022-02-17 15:36:49 -0300

    Update ManyToMany user_role relationship.

--- src/main/java/com/alkemy/ong/data/User.java
+++ src/main/java/com/alkemy/ong/data/User.java
@@ -30,9 +30,17 @@ public class User {
 
     private String photo;
 
-//    @ManyToMany
-//    @Column(name = "role_id")
-//    private Role roleId;
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(
+//            name = "user_role",
+//            joinColumns = {
+//                    @JoinColumn(name = "user_id", referencedColumnName = "id")
+//            },
+//            inverseJoinColumns = {
+//                    @JoinColumn(name = "role_id", referencedColumnName = "id")
+//            }
+//    )
+//    private List<Role> roles;
 
     @Column(name = "created_at")
     @CreatedDate
