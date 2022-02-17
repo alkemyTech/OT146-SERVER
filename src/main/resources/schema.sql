ee17ebfbbed38e102c8c352daef233199c1ac0b0
Diego Noblega <diegomendoza20015@gmail.com>
2022-02-17 11:58:19 -0300

    added table testimonials in schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,13 @@ create table if not exists lucas (
     name varchar(256) not null,
     age int
 );
+
+create table if not exists testimonials (
+    id int not null AUTO_INCREMENT,
+    name varchar(256) not null,
+    image varchar(256),
+    content varchar(500),
+    creation_date timestamp not null,
+    is_deleted bit(1) not null default 0,
+    primary key(id)
+);
