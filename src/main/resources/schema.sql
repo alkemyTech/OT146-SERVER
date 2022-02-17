ccc4b3827d7c247606c1205c633e5207742654eb
Emilio Ferraro <emilio_1986win@hotmail.com>
2022-02-17 19:54:39 -0300

    Db_Schema_Added

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,15 @@ create table if not exists lucas (
     name varchar(256) not null,
     age int
 );
+
+create table if not exists activities (
+
+    id int not null primary key AUTO_INCREMENT,
+    name: VARCHAR NOT NULL
+    content: VARCHAR NOT NULL
+    image: VARCHAR NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted: bit(1) NOT NULL default = 0
+
+);
