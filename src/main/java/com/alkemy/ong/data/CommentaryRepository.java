280f447b77acc1c25944f8db87ecbd80283c2e5d
Pablo Reyes <reyes_pabloeduardo@hotmail.com>
2022-02-17 18:01:37 -0300

    Add Commentary entity and repository

--- src/main/java/com/alkemy/ong/data/CommentaryRepository.java
+++ src/main/java/com/alkemy/ong/data/CommentaryRepository.java
@@ -1,2 +1,8 @@
-package com.alkemy.ong.data;public interface CommentaryRepository {
+package com.alkemy.ong.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentaryRepository extends JpaRepository<Commentary, Integer> {
 }
