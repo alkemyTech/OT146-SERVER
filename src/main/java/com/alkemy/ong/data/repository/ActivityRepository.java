59cbb1fc24a90ec2ba331941c939b706b2d356e9
Emilio Ferraro <emilio_1986win@hotmail.com>
2022-02-18 16:37:40 -0300

    longAddedinRepo

--- src/main/java/com/alkemy/ong/data/repository/ActivityRepository.java
+++ src/main/java/com/alkemy/ong/data/repository/ActivityRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<ActivityEntity, Integer> {
+public interface ActivityRepository extends JpaRepository<ActivityEntity, Long> {
 }
