4985e54b0be4eabab677e2ec873d5d2c5cf4ff32
Dante <dantejoaquin.mogni@gmail.com>
2022-02-18 10:17:01 -0300

    OT146-16 | created entity 'roles'

---
+++ src/main/java/com/alkemy/ong/data/entity/RolesEntity.java
@@ -0,0 +1,63 @@
+package com.alkemy.ong.data.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "roles")
+public class RolesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
