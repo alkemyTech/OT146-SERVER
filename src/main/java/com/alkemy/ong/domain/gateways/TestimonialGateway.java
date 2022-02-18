961718dcf62c539a5d7b2c4ca0a6db6c779bb1f4
Diego Noblega <diegomendoza20015@gmail.com>
2022-02-18 18:20:49 -0300

    added in domain the service and gateway

---
+++ src/main/java/com/alkemy/ong/domain/gateways/TestimonialGateway.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.domain.gateways;
+
+import com.alkemy.ong.domain.models.Testimonial;
+
+public interface TestimonialGateway {
+    public Testimonial create(Testimonial testimonial);
+}
---
+++ src/main/java/com/alkemy/ong/domain/services/TestimonialService.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.domain.services;
+
+
+import com.alkemy.ong.domain.gateways.TestimonialGateway;
+import com.alkemy.ong.domain.models.Testimonial;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestimonialService {
+    private TestimonialGateway testimonialGateway;
+
+    public TestimonialService(TestimonialGateway testimonialGateway) {
+        this.testimonialGateway = testimonialGateway;
+    }
+
+    public Testimonial save(Testimonial testimonial){
+        return testimonialGateway.create(testimonial);
+    }
+}
