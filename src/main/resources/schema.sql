cf0a0271bbbe0218ff8be09aae748bf1f4f21e73
Dante <dantejoaquin.mogni@gmail.com>
2022-02-18 09:59:13 -0300

    OT146-16 | updated sql script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,15 @@
+use alkemy_ong;
+
 create table if not exists lucas (
     id int not null primary key AUTO_INCREMENT,
     name varchar(256) not null,
     age int
+);
+
+create table if not exists roles (
+    id int not null primary key AUTO_INCREMENT,
+    name varchar(256) not null,
+	description varchar(256),
+    created_at timestamp not null,
+    updated_at timestamp
 );
