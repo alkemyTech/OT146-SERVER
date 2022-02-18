f949c14c2dfca16023a0154bf02f091161309404
Diego Noblega <diegomendoza20015@gmail.com>
2022-02-18 09:49:17 -0300

    changed repository name to TestimonialRepository

--- src/main/java/com/alkemy/ong/data/respository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/data/respository/TestimonialRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestimonialsRepository extends CrudRepository<Testimonial, Long> {
+public interface TestimonialRepository extends CrudRepository<Testimonial, Long> {
 }
