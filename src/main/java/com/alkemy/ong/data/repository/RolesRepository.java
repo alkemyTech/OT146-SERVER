864dc71760167603adfad52e5f56244c1859f112
Dante <dantejoaquin.mogni@gmail.com>
2022-02-18 10:21:52 -0300

    OT146-16 | removed unused import

--- src/main/java/com/alkemy/ong/data/repository/RolesRepository.java
+++ src/main/java/com/alkemy/ong/data/repository/RolesRepository.java
@@ -4,8 +4,6 @@ import com.alkemy.ong.data.entity.RolesEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RolesRepository extends CrudRepository<RolesEntity, Long> {
 }
