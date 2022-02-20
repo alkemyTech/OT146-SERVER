6f97032fb9950f65566b2fdd1dfffbb3102df76c
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-20 14:40:06 -0300

    Modified Service

--- src/main/java/com/alkemy/ong/domain/service/MemberService.java
+++ src/main/java/com/alkemy/ong/domain/service/MemberService.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.domain.service;
 
-import com.alkemy.ong.domain.Member;
+import com.alkemy.ong.domain.model.Member;
 
 public interface MemberService {
     Member save(Member memberDTO2MemberDomain);
--- src/main/java/com/alkemy/ong/domain/service/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/service/impl/MemberServiceImpl.java
@@ -1,9 +1,7 @@
 package com.alkemy.ong.domain.service.impl;
 
-import com.alkemy.ong.data.DefaultMemberGateway;
-import com.alkemy.ong.data.entity.MemberEntity;
-import com.alkemy.ong.data.repository.MemberRepository;
-import com.alkemy.ong.domain.Member;
+import com.alkemy.ong.domain.gateway.MemberGateway;
+import com.alkemy.ong.domain.model.Member;
 import com.alkemy.ong.domain.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,20 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberServiceImpl implements MemberService {
 
-    private MemberRepository memberRepository;
-    private DefaultMemberGateway memberGateway;
+    private MemberGateway memberGateway;
 
     @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository, DefaultMemberGateway memberGateway) {
-        this.memberRepository = memberRepository;
+    public MemberServiceImpl(MemberGateway memberGateway) {
         this.memberGateway = memberGateway;
     }
 
     @Override
     public Member save(Member member) {
-        MemberEntity entity = this.memberGateway.memberDomain2MemberEntity(member);
-        MemberEntity entitySaved = this.memberRepository.save(entity);
-        Member resultDomain = this.memberGateway.memberEntity2MemberDomain(entitySaved);
-        return resultDomain;
+        return this.memberGateway.create(member);
     }
 }
