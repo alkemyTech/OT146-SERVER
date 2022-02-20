7f043df4021e271dc9829da38f3480cc80665825
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-20 14:39:25 -0300

    Modified interface

--- src/main/java/com/alkemy/ong/data/gateway/DefaultMemberGateway.java
+++ src/main/java/com/alkemy/ong/data/gateway/DefaultMemberGateway.java
@@ -1,8 +1,10 @@
-package com.alkemy.ong.data;
+package com.alkemy.ong.data.gateway;
 
 import com.alkemy.ong.data.entity.MemberEntity;
-import com.alkemy.ong.domain.Member;
-import com.alkemy.ong.domain.service.MemberGateway;
+import com.alkemy.ong.data.repository.MemberRepository;
+import com.alkemy.ong.domain.model.Member;
+import com.alkemy.ong.domain.gateway.MemberGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -11,8 +13,22 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class DefaultMemberGateway implements MemberGateway {
 
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public DefaultMemberGateway(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
-    public MemberEntity memberDomain2MemberEntity(Member member) {
+    public Member create(Member model) {
+        MemberEntity entity = this.memberModel2MemberEntity(model);
+        MemberEntity entitySaved = this.memberRepository.save(entity);
+        return this.memberEntity2MemberModel(entitySaved);
+    }
+
+    //======================================================================================================
+    public MemberEntity memberModel2MemberEntity(Member member) {
         MemberEntity entity = new MemberEntity();
         entity.setName(member.getName());
         entity.setFacebookUrl(member.getFacebookUrl());
@@ -24,8 +40,7 @@ public class DefaultMemberGateway implements MemberGateway {
         return entity;
     }
 
-    @Override
-    public Member memberEntity2MemberDomain(MemberEntity entity) {
+    public Member memberEntity2MemberModel(MemberEntity entity) {
         Member member = new Member();
         member.setId(entity.getId());
         member.setName(entity.getName());
@@ -43,4 +58,5 @@ public class DefaultMemberGateway implements MemberGateway {
         LocalDate date = LocalDate.parse(stringDate, formatter);
         return date;
     }
+
 }
---
+++ src/main/java/com/alkemy/ong/domain/gateway/MemberGateway.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.domain.gateway;
+
+import com.alkemy.ong.domain.model.Member;
+
+public interface MemberGateway {
+
+    Member create(Member model);
+}
