6b51148fb88f82e5806e6cdb541642a4275f53c2
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-21 18:08:11 -0300

    Moved to exception package

---
+++ src/main/java/com/alkemy/ong/web/exception/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.web.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Arrays;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {FieldError.class})
+    protected ResponseEntity<Object> handleFieldException(RuntimeException ex, WebRequest request) {
+        ApiErrorDTO apiErrorDTO = new ApiErrorDTO(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                Arrays.asList("field cannot be empty")
+        );
+        return handleExceptionInternal(ex, apiErrorDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
