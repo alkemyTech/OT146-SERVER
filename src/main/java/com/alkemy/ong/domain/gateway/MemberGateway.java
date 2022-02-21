68d38a2a656da032c71c9d4145c4fcaefeea15f5
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-21 18:04:30 -0300

    Remove packages

--- src/main/java/com/alkemy/ong/domain/gateway/MemberGateway.java
+++
@@ -1,8 +0,0 @@
-package com.alkemy.ong.domain.gateway;
-
-import com.alkemy.ong.domain.model.Member;
-
-public interface MemberGateway {
-
-    Member create(Member model);
-}
--- src/main/java/com/alkemy/ong/domain/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.ong.domain.service;
-
-import com.alkemy.ong.domain.model.Member;
-
-public interface MemberService {
-    Member save(Member memberDTO2MemberDomain);
-}
