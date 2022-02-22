6b8f0e9f48e6e4c5618260c359957c80be66ba83
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-22 12:13:22 -0300

    implemented getAllMembers method

--- src/main/java/com/alkemy/ong/domain/members/MemberService.java
+++ src/main/java/com/alkemy/ong/domain/members/MemberService.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.domain.members;
 
+import java.util.List;
 
 public interface MemberService {
     Member save(Member memberDTO2MemberDomain);
+    List<Member> getAllMembers();
 }
--- src/main/java/com/alkemy/ong/domain/members/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/members/MemberServiceImpl.java
@@ -3,10 +3,12 @@ package com.alkemy.ong.domain.members;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MemberServiceImpl implements MemberService {
 
-    private MemberGateway memberGateway;
+    private final MemberGateway memberGateway;
 
     @Autowired
     public MemberServiceImpl(MemberGateway memberGateway) {
@@ -17,4 +19,10 @@ public class MemberServiceImpl implements MemberService {
     public Member save(Member member) {
         return this.memberGateway.create(member);
     }
+
+    @Override
+    public List<Member> getAllMembers() {
+        return this.memberGateway.findAll();
+    }
+
 }
