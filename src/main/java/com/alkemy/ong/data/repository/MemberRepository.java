8da5f8d4d195b29683c91fcc35580fb4720d108f
Felipe Herrera <herrerafelipe.093@gmail.com>
2022-02-22 12:14:42 -0300

    Modified

--- src/main/java/com/alkemy/ong/data/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/data/repository/MemberRepository.java
@@ -1,9 +1,9 @@
 package com.alkemy.ong.data.repository;
 
 import com.alkemy.ong.data.entity.MemberEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends CrudRepository<MemberEntity, Long> {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 }
