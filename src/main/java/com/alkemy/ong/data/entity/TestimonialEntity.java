a519bf1bedebbb7c69f8862f464c131ef3bb3970
Diego Noblega <diegomendoza20015@gmail.com>
2022-02-23 13:48:25 -0300

    implemented correct timestamp in entity

--- src/main/java/com/alkemy/ong/data/entity/TestimonialEntity.java
+++ src/main/java/com/alkemy/ong/data/entity/TestimonialEntity.java
@@ -1,7 +1,9 @@
 package com.alkemy.ong.data.entity;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -23,8 +25,10 @@ public class TestimonialEntity {
     private String image;
     @Column(name = "content")
     private String content;
+    @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
     @Column(name = "deleted")
