8fc4ce4d667d8d224484c1e2a20c9e1d1ab147ae
Esteban Geraldo <esugeraldo@gmail.com>
2022-03-02 17:20:08 -0300

    create endpoint listar contactos

--- src/main/java/com/alkemy/ong/data/gateways/DefaultContactGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultContactGateway.java
@@ -1,8 +1,54 @@
 package com.alkemy.ong.data.gateways;
 
+import com.alkemy.ong.data.entity.ContactEntity;
 import com.alkemy.ong.data.repository.ContactRepository;
+import com.alkemy.ong.domain.contacts.Contact;
+import com.alkemy.ong.domain.contacts.ContactGateway;
+import org.springframework.stereotype.Component;
 
-public class DefaultContactGateway {
+import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 
+@Component
+public class DefaultContactGateway implements ContactGateway {
+
+    private final ContactRepository contactRepository;
+
+    public DefaultContactGateway(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public Contact create(Contact contact) {
+        return toModel(contactRepository.save(toEntity(contact)));
+    }
+
+    @Override
+    public List<Contact> findAll() {
+        List<ContactEntity> contactEntityList = contactRepository.findAll();
+        return toModelList(contactEntityList);
+    }
+
+    private ContactEntity toEntity(Contact contact) {
+        return ContactEntity.builder()
+                .name(contact.getName())
+                .phone(contact.getPhone())
+                .email(contact.getEmail())
+                .message(contact.getMessage())
+                .build();
+    }
+
+    private Contact toModel(ContactEntity contactEntity) {
+        return Contact.builder()
+                .id(contactEntity.getId())
+                .name(contactEntity.getName())
+                .email(contactEntity.getEmail())
+                .message(contactEntity.getMessage())
+                .build();
+    }
+
+    private List<Contact> toModelList(List<ContactEntity> news) {
+        return news.stream().map(this::toModel).collect(toList());
+    }
 }
