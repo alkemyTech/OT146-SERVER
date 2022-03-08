3066909fc80b28a3120399fa8ade1f680cdf3c77
lucas.paoliello <FWAR00036@fulcrumdigital.com>
2022-03-08 09:46:44 -0300

    Removing gateway interface from Commentary Service

--- src/main/java/com/alkemy/ong/domain/comments/CommentaryService.java
+++ src/main/java/com/alkemy/ong/domain/comments/CommentaryService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CommentaryService implements CommentaryGateway {
+public class CommentaryService {
 
     private final CommentaryGateway gateway;
 
@@ -13,32 +13,26 @@ public class CommentaryService implements CommentaryGateway {
         this.gateway = gateway;
     }
 
-    @Override
     public Commentary create(Commentary commentary) {
         return gateway.create(commentary);
     }
 
-    @Override
     public List<Commentary> findAll() {
         return gateway.findAll();
     }
 
-    @Override
     public Commentary findById(Long id) {
         return gateway.findById(id);
     }
 
-    @Override
     public Commentary update(Commentary commentary) {
         return gateway.update(commentary);
     }
 
-    @Override
     public void delete(Long id) {
         gateway.delete(id);
     }
 
-    @Override
     public boolean existsById(Long id) {
         return gateway.existsById(id);
     }
