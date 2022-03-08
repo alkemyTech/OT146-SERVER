f3bf00fc574a88a987b2fa32101137c44737542e
dante <dantejoaquin.mogni@gmail.com>
2022-03-08 17:45:29 -0300

    OT146-57 | Added JPA method to get last slide by its order

--- src/main/java/com/alkemy/ong/data/repository/SlidesRepository.java
+++ src/main/java/com/alkemy/ong/data/repository/SlidesRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SlidesRepository extends JpaRepository<SlidesEntity, Long> {
+    SlidesEntity findTopByOrderBySlideOrderDesc();
 }
