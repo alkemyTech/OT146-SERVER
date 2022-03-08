000025d96e534b5eb33c367e4b563ee6f9044c4e
Diego Noblega <diegomendoza20015@gmail.com>
2022-03-08 20:44:12 -0300

    added equals override

--- src/main/java/com/alkemy/ong/data/entity/TestimonialEntity.java
+++ src/main/java/com/alkemy/ong/data/entity/TestimonialEntity.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "testimonials")
@@ -34,4 +35,12 @@ public class TestimonialEntity {
     @Column(name = "deleted")
     @Type(type = "numeric_boolean")
     private boolean deleted = Boolean.FALSE;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestimonialEntity that = (TestimonialEntity) o;
+        return isDeleted() == that.isDeleted() && Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName()) && Objects.equals(getImage(), that.getImage()) && Objects.equals(getContent(), that.getContent()) && Objects.equals(getCreatedAt(), that.getCreatedAt()) && Objects.equals(getUpdatedAt(), that.getUpdatedAt());
+    }
 }
