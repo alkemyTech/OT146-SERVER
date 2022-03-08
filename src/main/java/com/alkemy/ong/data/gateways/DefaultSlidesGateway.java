aba90aa3ba60158be24d282d357b2e3711982a80
dante <dantejoaquin.mogni@gmail.com>
2022-03-08 17:44:39 -0300

    OT146-57 | Implemented logic for decoding & storing file in AWS

--- src/main/java/com/alkemy/ong/data/gateways/DefaultSlidesGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultSlidesGateway.java
@@ -7,9 +7,13 @@ import com.alkemy.ong.data.repository.SlidesRepository;
 import com.alkemy.ong.domain.slides.SimpleSlide;
 import com.alkemy.ong.domain.slides.SlidesGateway;
 import com.alkemy.ong.domain.slides.Slides;
+import com.alkemy.ong.domain.storage.CustomMultipartFile;
+import com.alkemy.ong.domain.storage.StorageService;
 import com.alkemy.ong.web.exceptions.ResourceNotFoundException;
+import com.amazonaws.util.Base64;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -18,11 +22,15 @@ import static java.util.stream.Collectors.toList;
 public class DefaultSlidesGateway implements SlidesGateway {
     private final SlidesRepository slidesRepository;
     private final OrganizationRepository organizationRepository;
+    private final StorageService storageService;
 
-    public DefaultSlidesGateway(SlidesRepository slidesRepository, OrganizationRepository organizationRepository){
+    public DefaultSlidesGateway(SlidesRepository slidesRepository,
+                                OrganizationRepository organizationRepository,
+                                StorageService storageService){
 
         this.slidesRepository = slidesRepository;
         this.organizationRepository = organizationRepository;
+        this.storageService = storageService;
     }
 
     public static Slides toDomain(SlidesEntity slidesEntity){
@@ -73,13 +81,45 @@ public class DefaultSlidesGateway implements SlidesGateway {
 
     @Override
     public Slides create(SimpleSlide slides) {
-        // TODO: almacenar imagenes en Bucket Amazon S3
 
         OrganizationEntity organization = organizationRepository
                 .findById(slides.getOrganizationId())
                 .orElseThrow(() -> new ResourceNotFoundException("Cannot find organization with id: " + slides.getOrganizationId()));
 
+        // --- AMAZON AWS Subida de imagen
+
+        // Quito "data:image/jpeg;base64" para quedarme solo con los bytes a decodear
+        String trimmedEncodedImage = slides.getImageEncoded().substring(slides.getImageEncoded().indexOf(",") + 1);
+
+        byte[] decodedBytes = Base64.decode(trimmedEncodedImage);
+
+        // Nombre del archivo con su correspondiente extensión
+        String fileName = "slide.jpeg";
+        CustomMultipartFile customMultipartFile = new CustomMultipartFile(decodedBytes, fileName);
+
+        try {
+            customMultipartFile.transferTo(customMultipartFile.getFile());
+        } catch (IllegalStateException e) {
+            System.out.println("IllegalStateException : " + e);
+        } catch (IOException e) {
+            System.out.println("IOException : " + e);
+        }
+
+        // Almaceno la imagen en AWS y obtengo su correspondiente URL
+        String imageUrl = storageService.save(customMultipartFile).getUrl();
+        slides.setImageUrl(imageUrl);
+
         SlidesEntity slideEntity = simpleToEntity(slides, organization);
+
+        // Si no se especifica el orden del slide, busco el último valor y le sumo uno
+        if(slideEntity.getSlideOrder() == null){
+            slideEntity.setSlideOrder(
+                    slidesRepository
+                            .findTopByOrderBySlideOrderDesc()
+                            .getSlideOrder() + 1
+            );
+        }
+
         slideEntity.setDeleted(false);
 
         return toDomain(slidesRepository.save(slideEntity));
