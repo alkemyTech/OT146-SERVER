342d5ea567ca6f1e3b42220c86c133ee3cea0404
Pablo Reyes <reyes_pabloeduardo@hotmail.com>
2022-03-09 14:14:46 -0300

    Add RegisterTest

--- src/test/java/com/alkemy/ong/web/controller/UserControllerTest.java
+++ src/test/java/com/alkemy/ong/web/controller/UserControllerTest.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.web.controller;
 
 import com.alkemy.ong.data.entity.RolesEntity;
 import com.alkemy.ong.data.entity.UserEntity;
+import com.alkemy.ong.data.repository.RolesRepository;
 import com.alkemy.ong.data.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,12 +18,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,6 +35,12 @@ class UserControllerTest {
 
     @MockBean
     UserRepository userRepository;
+
+    @MockBean
+    RolesRepository roleRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
 
     @Test
@@ -94,6 +103,42 @@ class UserControllerTest {
                 .andExpect(jsonPath("$[0].id", is(111)))
                 .andExpect(jsonPath("$[1].id", is(555)));
     }
+
+    @Test
+    void testRegisterUser() throws Exception {
+        RolesEntity role = new RolesEntity();
+        role.setId(1L);
+        UserEntity registerUserMock = generateUserEntity(25L, "Test", "Register", "new@mail.com", "12345678", null);
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(registerUserMock);
+        when(roleRepository.findById(any())).thenReturn(Optional.of(role));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(registerUserMock));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(25)))
+                .andExpect(jsonPath("$.firstName", is("Test")))
+                .andExpect(jsonPath("$.lastName", is("Register")))
+                .andExpect(jsonPath("$.email", is("new@mail.com")));
+    }
+
+    @Test // NO SE COMO HACER PARA VERIFICAR QUE FUNCIONA LA EXCEPCION :(
+    void testEmailTaken() throws Exception {
+        when(userRepository.existsByEmail(any())).thenReturn(true);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/auth/register")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest());
+    }
+
 
     private UserEntity generateUserEntity(Long id, String name, String lastname, String email, String password, String photo) {
         return UserEntity.builder()
