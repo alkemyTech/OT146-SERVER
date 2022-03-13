adc4c0b7e869355016c4abe2bddb928294ef451b
Sol Piqueras <piquerasmariasol@gmail.com>
2022-03-13 17:17:21 -0300

    The show organization test has been created

--- src/test/java/com/alkemy/ong/web/controller/OrganizationControllerTest.java
+++ src/test/java/com/alkemy/ong/web/controller/OrganizationControllerTest.java
@@ -3,7 +3,9 @@ package com.alkemy.ong.web.controller;
 import com.alkemy.ong.data.entity.OrganizationEntity;
 import com.alkemy.ong.data.entity.SlidesEntity;
 import com.alkemy.ong.data.repository.OrganizationRepository;
+import com.alkemy.ong.domain.organization.Organization;
 import com.alkemy.ong.web.controller.OrganizationController.OrganizationDto;
+import com.alkemy.ong.web.controller.OrganizationController.OrganizationSimpleDto;
 import com.alkemy.ong.web.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -20,7 +22,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.toList;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,6 +44,28 @@ public class OrganizationControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Test
+    void get_success() throws Exception{
+        OrganizationSimpleDto dtoSimple = buildSimpleDto();
+        OrganizationEntity resultEntity = buildEntity( 1L);
+        when(ongRepository.findById(1L)).thenReturn(Optional.of(resultEntity));
+
+        mockMvc.perform(get(ROOT_PATH + "/public/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dtoSimple)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void get_NotFound()throws Exception{
+        OrganizationSimpleDto dtoSimple = buildSimpleDto();
+        when(ongRepository.findById(5L)).thenThrow(new ResourceNotFoundException("Organization not found"));
+
+        mockMvc.perform(get(ROOT_PATH + "/public/5")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dtoSimple)))
+                .andExpect(status().isNotFound());
+    }
 
     @Test
     @WithMockUser(roles = "ADMIN")
@@ -77,7 +103,7 @@ public class OrganizationControllerTest {
 
         when(ongRepository.findById(2L)).thenThrow(new ResourceNotFoundException("Organization not found"));
 
-        mockMvc.perform(put("/public/{id}", 2)
+        mockMvc.perform(put(ROOT_PATH + "/public/2")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dtoBody)))
                 .andExpect(status().isNotFound());
@@ -125,5 +151,20 @@ public class OrganizationControllerTest {
         List<SlidesController.SimpleSlideDto> slides = new ArrayList();
         ong.setSlides(slides);
         return ong;
+    }
+
+    private OrganizationSimpleDto buildSimpleDto(){
+        OrganizationSimpleDto simpleDto = OrganizationSimpleDto.builder()
+                .name("Test O-N-G")
+                .image("https://www.testong.com/image.png")
+                .address("Test 123")
+                .phone(1231231666)
+                .facebookLink("https://www.facebook.com/testong")
+                .instagramLink("https://www.instagram.com/testong")
+                .linkedinLink("https://www.linkedin.com/testong")
+                .build();
+        List<SlidesController.SimpleSlideDto> slides = new ArrayList();
+        simpleDto.setSlides(slides);
+        return simpleDto;
     }
 }
